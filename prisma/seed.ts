fa761522927f2cb1096a7ceee6b02e88b7ba84ab
daffa2555 <daffaanan11@gmail.com>
2026-08-25 03:05:15 +0800

    fix: use pg driver adapter for Prisma 7

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,29 @@
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 import bcrypt from 'bcryptjs'
+import 'dotenv/config'
 
-const prisma = new PrismaClient()
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
 
 async function main() {
   const hashedPassword = await bcrypt.hash('password', 10)
   
-  const superAdmin = await prisma.user.upsert({
+  const user = await prisma.user.upsert({
     where: { email: 'kabid.teknologi@himasti.org' },
     update: {},
     create: {
-      name: 'Kabid Teknologi',
       email: 'kabid.teknologi@himasti.org',
+      name: 'Ketua Bidang Teknologi',
       password: hashedPassword,
     },
   })
   
-  console.log('Super Admin created:', superAdmin)
+  console.log({ user })
 }
 
 main()
