4ef7582fb109a51b7c1726ad49caf82e6b500c44
daffa2555 <daffaanan11@gmail.com>
2026-08-25 01:17:29 +0800

    fix: enforce storage_path on vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -21,3 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+if (isset($_SERVER["APP_STORAGE_PATH"])) {
+    $app->useStoragePath($_SERVER["APP_STORAGE_PATH"]);
+}
+
+return $app;
