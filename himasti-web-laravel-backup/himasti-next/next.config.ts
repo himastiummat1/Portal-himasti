1a0f22287ff5d3606df7ff4fc32979c885719e4f
daffa2555 <daffaanan11@gmail.com>
2026-08-27 18:18:16 +0800

    fix: Revert unsupported eslint config

--- himasti-web-laravel-backup/himasti-next/next.config.ts
+++ himasti-web-laravel-backup/himasti-next/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },
   images: {
     domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
   },
