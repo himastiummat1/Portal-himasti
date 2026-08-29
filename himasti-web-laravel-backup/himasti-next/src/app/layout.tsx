3f6e989e57298c652282a9f4d5c617798a9ec17a
Vidyax <138625503+daffa2555@users.noreply.github.com>
2026-08-30 07:28:09 +0800

    Update layout.tsx

--- himasti-web-laravel-backup/himasti-next/src/app/layout.tsx
+++ himasti-web-laravel-backup/himasti-next/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navigation } from "@/components/layout/Navigation";
+import Navigation from "@/components/layout/Navigation";
 import FloatingChatbot from "@/components/chat/FloatingChatbot";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "HIMASTI Web",
   description: "Portal Informasi HIMASTI",
+  verification: {
+    google: "nA1ksDi8zu990WSLCnvaFdYoZ59ewc0d9jrtciqCOzk",
+  },
 };
 
 export default function RootLayout({
@@ -18,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id">
-      <body className={`${inter.className} font-sans antialiased text-gray-900  bg-gray-100 `}>
+      <body className={`${inter.className} font-sans antialiased text-gray-900 bg-gray-100`}>
         <div className="min-h-screen">
           <Navigation />
           <main>{children}</main>
